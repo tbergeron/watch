6eb32d660bb888fdfe3060d78429c29873afe383
Tommy Bergeron <t.bergeron@gmail.com>
2012-12-02 01:52:42 -0500

    ehehe

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -60,7 +60,7 @@ module.exports = ThinAir.createController({
                     name = null;
 
                 phantomjs.stdout.on('data', function (data) {
-                    name = data;
+                    name = data.toString().replace("\n", "");
                     console.log('stdout: ' + data);
                 });
 
@@ -71,14 +71,14 @@ module.exports = ThinAir.createController({
                 phantomjs.on('exit', function (code) {
                     params.name = name;
 
-                    fs.writeFileSync('/home/ubuntu/watch/TABARNAK', __dirname + '/public/screenshots/' + name.toString().replace("\n", "") + '.png');
-                    fs.writeFileSync('/home/ubuntu/watch/TABARNAK', __dirname + '/public/screenshots/' + name.toString().replace("\n", "") + '_cropped.png');
+                    fs.writeFileSync('/home/ubuntu/watch/TABARNAK', '/public/screenshots/' + name + '.png');
+                    fs.writeFileSync('/home/ubuntu/watch/TABARNAK', '/public/screenshots/' + name + '_cropped.png');
 
                     console.log('cropping ', name);
 
                     var image = new Magician(
-                        path.join(__dirname, 'public/screenshots/' + name.toString().replace("\n", "") + '.png'),
-                        path.join(__dirname, 'public/screenshots/' + name.toString().replace("\n", "") + '_cropped.png'));
+                        path.join(__dirname, 'public/screenshots/' + name + '.png'),
+                        path.join(__dirname, 'public/screenshots/' + name + '_cropped.png'));
 
                     fs.writeFileSync('/home/ubuntu/watch/TABARNAK', 'entre deux');
 
