9520247243a04af4bbb21eb7a00792d104db3557
Tommy Bergeron <t.bergeron@gmail.com>
2012-12-01 23:10:08 -0500

    can now take screenshots of urls

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,7 +1,34 @@
-var ThinAir = require('thinair');
+var ThinAir = require('thinair'),
+    spawn = require('child_process').spawn;
 
 module.exports = ThinAir.createController({
     index: function(req, res, params) {
         this.sendTemplate(req, res, 'index', params);   
+    },
+
+    // todo: this should loop on a database and take screenshots each X minutes
+    take_screenshot: function(req, res, params) {
+        if ((this.isPost(req)) && (params.post.url)) {
+            var phantomjs = spawn('phantomjs', ['screenshot.js', params.post.url]),
+                that = this,
+                name = null;
+
+            phantomjs.stdout.on('data', function (data) {
+                name = data;
+                console.log('stdout: ' + data);
+            });
+
+            phantomjs.stderr.on('data', function (data) {
+                console.log('stderr: ' + data);
+            });
+
+            phantomjs.on('exit', function (code) {
+                params.name = name;
+                that.sendTemplate(req, res, 'view', params);
+            });
+        } else {
+            params.error = true;
+            this.sendTemplate(req, res, 'view', params);
+        }
     }
 });
--- screenshot.js
+++ screenshot.js
@@ -1,10 +1,29 @@
-//create new webpage object
-var page = new webpage();
+var system = require('system');
 
-//load the page
-page.open('http://espn.go.com/nfl', function (status) {
-    //fire callback to take screenshot after load complete
-    page.render('espn.png');
-    //finish
-    phantom.exit();
-});
+if (url = system.args[1]) {
+    var page = new WebPage();
+
+    page.open(url, function (status) {
+        var date = new Date(),
+            filename = url.replace('http://www.', ''),
+            filename = filename.replace('http://', ''),
+            filename = filename.replace(/[:/.?=]/g, ''),
+            filename = filename + '_' + getFullDate(date);
+
+        page.render('public/screenshots/' + filename + '.png');
+
+        console.log(filename);
+
+        phantom.exit();
+    });
+}
+
+function getFullDate(d){
+    function pad(n){return n<10 ? '0'+n : n}
+    return d.getFullYear()+'-'
+        + pad(d.getMonth()+1)+'-'
+        + pad(d.getDate())+'_'
+        + pad(d.getHours())+'-'
+        + pad(d.getMinutes())+'-'
+        + pad(d.getSeconds())+'';
+}
