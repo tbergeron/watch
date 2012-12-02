99d8c30ea39a1c00083d379d9f32615aac540e8b
Tommy Bergeron <t.bergeron@gmail.com>
2012-12-02 01:41:55 -0500

    test

--- screenshot.js
+++ screenshot.js
@@ -1,29 +1,29 @@
-var system = require('system');
-
-if (url = system.args[1]) {
-    var page = new WebPage();
-
-    page.open(url, function (status) {
-        var date = new Date(),
-            filename = url.replace('http://www.', ''),
-            filename = filename.replace('http://', ''),
-            filename = filename.replace(/[:/.?=]/g, ''),
-            filename = filename + '_' + getFullDate(date);
-
-        page.render('public/screenshots/' + filename + '.png');
-
-        console.log(filename);
-
-        phantom.exit();
-    });
-}
-
-function getFullDate(d){
-    function pad(n){return n<10 ? '0'+n : n}
-    return d.getFullYear()+'-'
-        + pad(d.getMonth()+1)+'-'
-        + pad(d.getDate())+'_'
-        + pad(d.getHours())+'-'
-        + pad(d.getMinutes())+'-'
-        + pad(d.getSeconds())+'';
-}
+//var system = require('system');
+//
+//if (url = system.args[1]) {
+//    var page = new WebPage();
+//
+//    page.open(url, function (status) {
+//        var date = new Date(),
+//            filename = url.replace('http://www.', ''),
+//            filename = filename.replace('http://', ''),
+//            filename = filename.replace(/[:/.?=]/g, ''),
+//            filename = filename + '_' + getFullDate(date);
+//
+//        page.render('public/screenshots/' + filename + '.png');
+//
+//        console.log(filename);
+//
+//        phantom.exit();
+//    });
+//}
+//
+//function getFullDate(d){
+//    function pad(n){return n<10 ? '0'+n : n}
+//    return d.getFullYear()+'-'
+//        + pad(d.getMonth()+1)+'-'
+//        + pad(d.getDate())+'_'
+//        + pad(d.getHours())+'-'
+//        + pad(d.getMinutes())+'-'
+//        + pad(d.getSeconds())+'';
+//}
