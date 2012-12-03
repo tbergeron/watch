18e64caaec9ae4475a8b2e681b21c35a5fd1b2b2
Tommy Bergeron <t.bergeron@gmail.com>
2012-12-03 01:02:30 -0500

    eh

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -63,36 +63,35 @@ module.exports = ThinAir.createController({
 
         this.Websites.getAll(function(websites) {
             var outputString = '',
-                that = this,
-                i = 0;
+                that = this;
 
             websites.forEach(function(website) {
-                i = i + 1;
-                console.log('up', i, website.domain);
                 var phantomjs = spawn('/usr/local/bin/phantomjs', ['/home/ubuntu/watch/screenshot.js', website.url]),
                     that = this;
 
                 phantomjs.stdout.on('data', function (data) {
-                    if (data.indexOf('fail') !== -1) {
-                        var split = name.split(' '),
-                            name = split[1];
+                    if (data) {
+                        if (data.indexOf('fail') !== -1) {
+                            var split = name.split(' '),
+                                name = split[1];
 
-                        console.log('split', split);
+                            console.log('split', split);
 
-                        fs.createReadStream(path.join(__dirname, '../../public/img/offline.jpg'))
-                            .pipe(fs.createWriteStream(path.join(__dirname, '../../public/screenshots/' + name + '.png')));
-                    } else {
-                        var name = data.toString().replace("\n", "");
+                            fs.createReadStream(path.join(__dirname, '../../public/img/offline.jpg'))
+                                .pipe(fs.createWriteStream(path.join(__dirname, '../../public/screenshots/' + name + '.png')));
+                        } else {
+                            var name = data.toString().replace("\n", "");
 
-                        var image = new Magician(
-                            path.join(__dirname, '../../public/screenshots/' + name + '.png'),
-                            path.join(__dirname, '../../public/screenshots/' + name + '_cropped.png'));
+                            var image = new Magician(
+                                path.join(__dirname, '../../public/screenshots/' + name + '.png'),
+                                path.join(__dirname, '../../public/screenshots/' + name + '_cropped.png'));
 
-                        image.crop({x: 0, y: 0, width: 1080, height: 720}, function(err) {
+                            image.crop({x: 0, y: 0, width: 1080, height: 720}, function(err) {
 //                       if (err) console.error('Magician error: ', err);
-                        });
+                            });
 
-                        console.log('Took screenshot: ', name);
+                            console.log('Took screenshot: ', name);
+                        }
                     }
                 });
 
